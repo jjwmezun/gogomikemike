6a0b17cc9626e7aff50cf386acaeacaf9cf0f746
jjwmezun <jjwmezun@gmail.com>
2018-10-01 23:21:11 -0700

    Add some things that do not work

--- header.php
+++ header.php
@@ -3,10 +3,10 @@
 	<head>
 		<?php gomike_html_title(); ?>
 		<link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,900,700' rel='stylesheet' type='text/css'>
+		<link rel="canonical" href="<?php echo ( ( is_home() ) ? home_url() : get_permalink( get_the_ID() ) ); ?>">
 		<?php wp_head(); ?>
 	</head>
 	<body <?php gomike_body_class(); ?>>
-
 		<header class="gomike-main-header">
 			<h1 class="gomike-main-header-title"><?php bloginfo( 'name' ); ?></h1>
 			<?php include( 'assets/img/microstories-logo.svg' ); ?>
