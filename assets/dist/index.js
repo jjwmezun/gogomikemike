a43893f6ff49a6230b06d9d7a531b17fcc113170
J. J. W. Mezun <jjwmezun@gmail.com>
2023-09-21 03:02:52 -0700

    Update color-randomization script

--- assets/dist/index.js
+++ assets/dist/index.js
@@ -1,1 +1,44 @@
-var Palette=[{name:"--MAINBG",base:224,s:40,l:80},{name:"--MAIN1",base:224,s:99,l:30},{name:"--MAIN2",base:224,s:84,l:40},{name:"--MAIN3",base:224,s:76,l:50},{name:"--MAIN4",base:224,s:60,l:60},{name:"--MAIN5",base:224,s:44,l:70}];function randInt(e,n){return Math.floor(Math.random()*(n-e))+e}function randByte(){return randInt(0,255)}function randomColor(){for(var e="hsl( ",n=0;n<3;n++)e+=randInt(0,100),n<2&&(e+=", ");return e+=" )"}function randHue(){return randInt(0,360)}function makeColor(e,n,a){return"hsl( "+e+", "+n+"%, "+(a-25)+"% )"}function adjustPercent(e,n){return 100<(e+=n)?e=100:e<0&&(e=0),e}function generateRandomPalette(){for(var e=randHue(),n=randInt(0,50)-45,a=randInt(0,30)-5,t=0;t<Palette.length;t++)document.body.style.setProperty(Palette[t].name,makeColor(e,adjustPercent(Palette[t].s,n),adjustPercent(Palette[t].l,a)))}sessionStorage.getItem("rand_pal_page_loaded")&&generateRandomPalette(),sessionStorage.setItem("rand_pal_page_loaded",!0);
+(() => {
+    const palette =
+    [
+        { name: '--MAINBG', base: 224, s: 40, l: 100 },
+        { name: '--MAIN1',  base: 224, s: 99, l:  20 },
+        { name: '--MAIN2',  base: 224, s: 84, l:  30 },
+        { name: '--MAIN3',  base: 224, s: 76, l:  50 },
+        { name: '--MAIN4',  base: 224, s: 60, l:  60 },
+        { name: '--MAIN5',  base: 224, s: 48, l:  80 }
+    ];
+
+    const randInt = ( min, max ) => Math.floor( Math.random() * ( max - min ) ) + min;
+
+    const makeColor = ( h, s, l ) => `hsl( ${ h }, ${ s }%, ${ l }% )`;
+
+    // Add adjust to value & keep within bounds o’ 0 - 100.
+    const adjustPercent = ( v, adjust ) => Math.max( 0, Math.min( 100, v + adjust ) );
+
+    const generateRandomPalette = () => {
+        const hue = randInt( 0, 360 );
+        const saturationAdjust = randInt( -45, 5 );
+        const lightAdjust = randInt( -20, -5 );
+
+        palette.forEach( color => {
+            document.body.style.setProperty(
+                color.name,
+                makeColor(
+                    hue,
+                    adjustPercent( color.s, saturationAdjust ),
+                    adjustPercent( color.l, lightAdjust )
+                )
+            );
+        });
+    };
+
+    // Only randomize palette on subsequent page loads.
+    if ( sessionStorage.getItem( 'rand_pal_page_loaded' ) ) {
+        // Randomize palette on DOM load.
+        document.addEventListener( `DOMContentLoaded`, generateRandomPalette );
+    }
+    else {
+        sessionStorage.setItem( 'rand_pal_page_loaded', true )
+    }
+})();
--- assets/js/random-palette.js
+++ assets/js/random-palette.js
@@ -1,79 +1,44 @@
-var Palette =
-[
-	{ 'name': '--MAINBG', 'base': 224, 's': 40, 'l': 80 },
-	{ 'name': '--MAIN1', 'base': 224, 's': 99, 'l': 30 },
-	{ 'name': '--MAIN2', 'base': 224, 's': 84, 'l': 40 },
-	{ 'name': '--MAIN3', 'base': 224, 's': 76, 'l': 50 },
-	{ 'name': '--MAIN4', 'base': 224, 's': 60, 'l': 60 },
-	{ 'name': '--MAIN5', 'base': 224, 's': 44, 'l': 70 }
-];
+(() => {
+    const palette =
+    [
+        { name: '--MAINBG', base: 224, s: 40, l: 100 },
+        { name: '--MAIN1',  base: 224, s: 99, l:  20 },
+        { name: '--MAIN2',  base: 224, s: 84, l:  30 },
+        { name: '--MAIN3',  base: 224, s: 76, l:  50 },
+        { name: '--MAIN4',  base: 224, s: 60, l:  60 },
+        { name: '--MAIN5',  base: 224, s: 48, l:  80 }
+    ];
 
-function randInt( min, max )
-{
-	return Math.floor( Math.random() * ( max - min ) ) + min;
-};
+    const randInt = ( min, max ) => Math.floor( Math.random() * ( max - min ) ) + min;
 
-function randByte()
-{
-	return randInt( 0, 255 );
-};
+    const makeColor = ( h, s, l ) => `hsl( ${ h }, ${ s }%, ${ l }% )`;
 
-function randomColor()
-{
-	var string = 'hsl( ';
+    // Add adjust to value & keep within bounds o’ 0 - 100.
+    const adjustPercent = ( v, adjust ) => Math.max( 0, Math.min( 100, v + adjust ) );
 
-	for ( var i = 0; i < 3; i++ )
-	{
-		string += randInt( 0, 100 );
+    const generateRandomPalette = () => {
+        const hue = randInt( 0, 360 );
+        const saturationAdjust = randInt( -45, 5 );
+        const lightAdjust = randInt( -20, -5 );
 
-		if ( i < 2 )
-		{
-			string += ', ';
-		}
-	}
+        palette.forEach( color => {
+            document.body.style.setProperty(
+                color.name,
+                makeColor(
+                    hue,
+                    adjustPercent( color.s, saturationAdjust ),
+                    adjustPercent( color.l, lightAdjust )
+                )
+            );
+        });
+    };
 
-	string += ' )';
-
-	return string;
-};
-
-function randHue()
-{
-	return randInt( 0, 360 );
-};
-
-function makeColor( h, s, l )
-{
-	return 'hsl( ' + h + ', ' + s + '%, ' + ( l - 25 ) + '% )';
-};
-
-function adjustPercent( val, adjust )
-{
-	val += adjust;
-
-	if ( val > 100 )
-		val = 100;
-	else if ( val < 0 )
-		val = 0;
-
-	return val;
-};
-
-function generateRandomPalette()
-{
-	var hue = randHue();
-	var sat_adjust = randInt( 0, 50 ) - 45; // -45 to 5
-	var light_adjust = randInt( 0, 30 ) - 5; // -5 to 25
-
-	for ( var i = 0; i < Palette.length; i++ )
-	{
-		document.body.style.setProperty( Palette[ i ][ 'name' ], makeColor( hue, adjustPercent( Palette[ i ][ 's' ], sat_adjust ), adjustPercent( Palette[ i ][ 'l' ], light_adjust ) ) );
-	}
-};
-
-if ( sessionStorage.getItem( 'rand_pal_page_loaded' ) )
-{
-	generateRandomPalette();
-}
-
-sessionStorage.setItem( 'rand_pal_page_loaded', true )
+    // Only randomize palette on subsequent page loads.
+    if ( sessionStorage.getItem( 'rand_pal_page_loaded' ) ) {
+        // Randomize palette on DOM load.
+        document.addEventListener( `DOMContentLoaded`, generateRandomPalette );
+    }
+    else {
+        sessionStorage.setItem( 'rand_pal_page_loaded', true )
+    }
+})();
