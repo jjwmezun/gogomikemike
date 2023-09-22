71009c2f758f1e28b2803d3a77d117d11160d477
J. J. W. Mezun <jjwmezun@gmail.com>
2023-09-22 01:19:52 -0700

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<h1 class="gomike-piece-title gomike-page-title"><?= __( '¡Page not found!', 'gogomikemike' ); ?></h1>
+			
+<div class="gomike-piece-content gomike-page-content">
+    <p><?= __( 'Sorry, this page doesn’t exist.', 'gogomikemike' ); ?></p>
+    <p><a href="/"><?= __( 'Return home', 'gogomikemike' ); ?></a></p>
+</div>
+
+<?php get_footer(); ?>
